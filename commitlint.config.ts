52e0536fcbee5cc90bd8fb83744356228763c179
Ignacio Bockl <ignaciobockl96@gmail.com>
2024-02-26 00:47:49 -0300

    husky and commit lint are added to maintain comment control

---
+++ commitlint.config.ts
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'perf',
+        'chore',
+        'build',
+        'ci',
+        'docs',
+        'refactor',
+        'style',
+        'test',
+        'merge',
+      ],
+    ],
+    'header-min-length': [2, 'always', 10],
+    'header-max-length': [2, 'always', 120],
+  },
+};
